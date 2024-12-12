d2474322355f99c3a15415bf31a77af012957644
ShinMyeongseop28 <ShinMyeongseop28@github.com>
2024-12-12 16:58:59 +0900

    ES6 자바스크립트 2

---
+++ LEARN-JAVASCRIPT/lectures/3_javascript/variable.js
@@ -0,0 +1,35 @@
+'use strict';
+number = 2;
+console.log(number);
+
+// let teacher = '짐코딩';
+// console.log(teacher);
+// teacher = '홍길동';
+// console.log(teacher);
+
+// // let teacher = '김길동'
+
+// const username = '짐코딩';
+// console.log(username);
+// username = 'change'
+
+// {
+//   let a = '변수';
+//   console.log('a:', a);
+//   {
+//     let c = 'c변수';
+//     console.log('c블록 a:', a);
+//   }
+// }
+// // console.log('a:', a);
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+// console.log('result: ', result);
+
+// console.log(sum(1, 2));
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }

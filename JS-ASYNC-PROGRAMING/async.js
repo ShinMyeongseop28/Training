176c0339a8197aee5f436de9ae46d0ce38aed834
ShinMyeongseop28 <ShinMyeongseop28@github.com>
2024-12-17 17:26:06 +0900

    자바스크립트 동기/비동기

---
+++ JS-ASYNC-PROGRAMING/async.js
@@ -0,0 +1,20 @@
+// 동기 프로그래밍
+function syncFunction() {
+  console.log('첫 번째 작업');
+  console.log('두 번째 작업');
+  console.log('세 번째 작업');
+}
+syncFunction();
+
+console.log('------------');
+// 비동기 프로그래밍
+function asyncFunction() {
+  console.log('첫 번째 작업');
+
+  setTimeout(() => {
+    console.log('두 번째 작업');
+  }, 1000);
+
+  console.log('세 번째 작업');
+}
+asyncFunction();
